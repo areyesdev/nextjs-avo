c15ce00d0394dfb1ea7e4dd2e9a1d49dfaa0092c
Andres Reyes <ar97yf@gmail.com>
2020-12-22 21:24:32 -0500

    [ADD] Tproduct

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Navbar from '../components/Navbar/Navbar'
 
 const HomePage = () => {
+  const [productList, setProductList] = useState<TProduct[]>([])
+  useEffect(() => {
+    window
+      .fetch('/api/avo')
+      .then((response) => response.json())
+      .then(({ data }: TAPIAvoResponse) => {
+        setProductList(data)
+      })
+  }, [])
+
   return (
     <div>
       <Navbar />
-      <div>Platzi and Next.js!</div>
+      <div>Platzi and NextJS</div>
+      {productList.map((product) => (
+        <div>{product.name}</div>
+      ))}
     </div>
   )
 }
--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
+import Navbar from '../../components/Navbar/Navbar'
 
 const ProductPage = () => {
+  const [product, setProduct] = useState<TProduct | null>(null)
   const { query } = useRouter()
-
+  useEffect(() => {
+    if (query.id) {
+      window
+        .fetch(`/api/avo/${query.id}`)
+        .then((response) => response.json())
+        .then((data: TProduct) => {
+          setProduct(data)
+        })
+    }
+  }, [query.id])
   return (
     <section>
-      <h1>Página producto: {query.id}</h1>
+      <Navbar />
+      <h1>Product page: </h1>
+      <div>{product?.name}</div>
     </section>
   )
 }
